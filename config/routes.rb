b0df0d308e0bb62b4957efc95e7a2c9ea41e2de0
Jaime <jaivalz@yahoo.com>
2016-03-12 17:00:57 +0100

    Added projects and tasks controllers and views

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,17 @@
 Rails.application.routes.draw do
+
+
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
   devise_for :users
+
   get 'pages/about'
 
-  root 'pages#about'
+  root 'project#index'
+
+  resources :project do
+    resources :task, only: [:show]
+  end
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
