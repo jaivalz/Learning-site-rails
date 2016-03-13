406bb22f0eb4ad2511e4ad03ad486595d5a4b124
Jaime <jaivalz@yahoo.com>
2016-03-13 23:39:55 +0100

    Updated project and task controllers

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,11 +1,7 @@
 module ApplicationHelper
 	def avatar_url(user)
 		gravatar_id = Digest::MD5::hexdigest(user.email).downcase
-		if user.image
-			user.image
-		else
 			"https://www.gravatar.com/avatar/#{gravatar_id}.jpg?d=identicon&s=40"
-		end
 	end
 
 	class CodeRayify < Redcarpet::Render::HTML
